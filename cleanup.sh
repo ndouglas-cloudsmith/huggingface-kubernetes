989dc621944ff62a59b155b2765e4e0123844f6a
Nigel Douglas <ndouglas@cloudsmith.io>
2026-01-09 14:01:33 +0000

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -5,7 +5,7 @@ OWNER="acme-corporation"
 REPOSITORY="acme-repo-one"
 
 # Define multiple tags in an array
-TAGS=("transformer" "feature-extraction" "stable-diffusion" "classifier" "facebook" "causal-lm" "llm" "multilingual" "text-generation" "mms" "pytorch")
+TAGS=("transformer" "feature-extraction" "stable-diffusion" "sae" "classifier" "facebook" "causal-lm" "llm" "multilingual" "text-generation" "mms" "pytorch")
 
 PAGE_SIZE=250
 BASE_URL="https://api.cloudsmith.io/v1/packages/${OWNER}/${REPOSITORY}/"
