2673e14c30e9fd84b01d080b005213e310a9f7d4
Nigel Douglas <ndouglas@cloudsmith.io>
2026-01-12 17:18:08 +0000

    Update make_bad_model.py

--- make_bad_model.py
+++ make_bad_model.py
@@ -1,10 +1,32 @@
 import pickle
 import os
+import webbrowser
 
-class Malicious:
+# --- 1. CRITICAL: Shell Access ---
+class CriticalMalicious:
     def __reduce__(self):
-        # This tells Python: "When you open me, run 'echo Hello World'"
-        return (os.system, ("echo 'This model is insecure!'",))
+        return (os.system, ("echo 'CRITICAL DETECTED'",))
 
-with open("my_test_model.pkl", "wb") as f:
-    pickle.dump(Malicious(), f)
+# --- 2. HIGH: Network/Browser Interaction ---
+class HighRisk:
+    def __reduce__(self):
+        return (webbrowser.open, ("http://malicious-site.com",))
+
+# --- 3. MEDIUM: Unsafe Keras-style patterns ---
+# Note: Medium is often triggered by Lambda layers in .h5 files,
+# but using a non-standard global in Pickle often defaults to Medium.
+class MediumRisk:
+    def __reduce__(self):
+        return (print, ("Potential unsafe logging",))
+
+# Save the files
+with open("test_critical.pkl", "wb") as f:
+    pickle.dump(CriticalMalicious(), f)
+
+with open("test_high.pkl", "wb") as f:
+    pickle.dump(HighRisk(), f)
+
+with open("test_medium.pkl", "wb") as f:
+    pickle.dump(MediumRisk(), f)
+
+print("Files created: test_critical.pkl, test_high.pkl, test_medium.pkl")
